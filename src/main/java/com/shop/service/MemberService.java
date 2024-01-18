5f5b73198ddd3d05dfb0d91143dd2fe5889bd530
Songinha <dlsgk91115@naver.com>
2024-01-19 06:05:57 +0900

    xzcxz

--- src/main/java/com/shop/service/MemberService.java
+++ src/main/java/com/shop/service/MemberService.java
@@ -4,6 +4,7 @@ import com.shop.dto.MemberUpdateDto;
 import com.shop.entity.Member;
 import com.shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -35,22 +39,23 @@ public class MemberService implements UserDetailsService {
             throw new IllegalStateException("이미 가입된 회원입니다.");
         }
     }
-
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {      // UserDetailsService 인터페이스의 loadUserByUsername() 메소드를 오버라이딩. 로그인할 유저의 email을 파라미터로 받음.
         Member member = memberRepository.findByEmail(email);
 
-        if(member == null){
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        if(member == null) {
             throw new UsernameNotFoundException(email);
         }
 
-        return User.builder()
+        return User.builder()                           // UserDetail을 구현하고 있는 User 객체를 반환해줌. User 객체를 생성하기 위해 생성자로 회원의 이메일, 비밀번호, role을 파라미터로 넘겨줌.
                 .username(member.getEmail())
                 .password(member.getPassword())
                 .roles(member.getRole().toString())
                 .build();
     }
+
 
 
     @ResponseBody
@@ -84,6 +89,8 @@ public class MemberService implements UserDetailsService {
 
 
     }
+
+
 
 
 
